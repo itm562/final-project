b33e85529b1155e5f7277532b43fdd78d3bd3d9b
Gauri GovindRajulu <Gauri.GovindRajulu@deltak-innovation.com>
2018-12-04 01:20:38 -0600

    Modified classes

--- shopping-cart/shopping-cart/routes/index.js
+++ shopping-cart/shopping-cart/routes/index.js
@@ -18,6 +18,20 @@ Product.find(function(err,docs){
 });
  
 });
+
+router.get('/sort', function(req, res, next) {
+  Product.find(function(err,docs){
+    var productChunk = [];
+  
+    var chunkSize = 3;
+    for(var i = 0; i < docs.length; i+= chunkSize){
+      docs = docs.sort({price: -1});
+      productChunk.push(docs.slice(i,i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunk });
+  });
+   
+});
 router.get('/user/signup',function(req,res,next){
   res.render('user/signup',{csrfToken: req.csrfToken()  });
 
